6d6f5148d0fb0a1f4d64a671908dc5f6b1d985de
Tomás Wolf <tomas.wolf.vb@gmail.com>
2023-07-01 20:29:49 -0400

    adds webhook with the response, haven't tested sorry but i'm dying at this point

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 import logging
+import os
 
+import httpx
 from fastapi import FastAPI
 from rq import Queue
 
@@ -21,30 +23,59 @@ def index() -> str:
 
 
 @app.post(
-    "/factorial", tags=["factorial"], operation_id="factorial", response_model=str
+    "/factorial",
+    tags=["factorial"],
+    operation_id="factorial",
+    response_model=str,
 )
-def factorial(number: int) -> str:
+def factorial(number: int, endpoint: str | None) -> str:
     """Factorial endpoint."""
-    # sleep for 5 seconds
-    job = q.enqueue(calc_factorial, number)
+    job = q.enqueue(factorial_job, number, endpoint)
     return str(job.id)
+
+
+@app.post("/webhook", tags=["webhook"], operation_id="webhook")
+async def webhook(result: int) -> None:
+    """Webhook endpoint."""
+    logger.info("The webhook was hit, result was: %d", result)
 
 
 _TOO_BIG = "ran out of memory bro"
 _NEGATIVE = "Number must be positive"
 
 
-def calc_factorial(number: int) -> str:
+async def factorial_job(number: int, url: str | None = None) -> None:
     """Calculate factorial."""
     logger.info("Calculating factorial for %d", number)
     if number < 0:
         logger.warning("Result is '%s'", _NEGATIVE)
-        return _NEGATIVE
+        return
     if number > 10:
         logger.warning("Result is '%s'", _TOO_BIG)
-        return _TOO_BIG
+        return
+
+    result = calc_factorial(number)
+
+    if url is None and is_production():
+        logger.info("No webhook url provided")
+        return
+    if url is None:
+        url = "http://localhost:3000/webhook"
+    headers = {"Content-Type": "application/json"}
+    async with httpx.AsyncClient() as client:
+        await client.post(url, data={"result": str(result)}, headers=headers)
+        logger.info("Webhook sent to %s", url)
+
+
+def calc_factorial(number: int) -> int:
+    """Calculate factorial."""
     result = 1
     for i in range(1, number + 1):
         result *= i
     logger.warning("Result is '%d'", result)
-    return str(result)
+    return result
+
+
+def is_production() -> bool:
+    """Check if we are in production."""
+    return os.getenv("APP_ENV", "development") == "production"
