e81a2e16667b0415347ba68afb131555fdf51d74
Tomás Wolf <tomas.wolf.vb@gmail.com>
2023-07-02 00:29:50 -0400

    test pydantic model

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import logging
 from typing import Optional
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 from rq import Queue
 
 from app.factorial_service.jobs import factorial_job
@@ -14,6 +15,29 @@ app = FastAPI()
 
 redis_instance = get_redis_client()
 q = Queue(connection=redis_instance)
+
+
+class MyModel(BaseModel):
+    """MyModel."""
+
+    name: str
+    age: int
+
+    class Config:
+        """Config."""
+
+        schema_extra = {
+            "example": {
+                "name": "John Doe",
+                "age": 42,
+            }
+        }
+
+
+class ResModel(BaseModel):
+    """ResModel."""
+
+    msg: str
 
 
 @app.get("/", tags=["health"], operation_id="check", response_model=str)
@@ -38,3 +62,10 @@ def factorial(number: int, endpoint: Optional[str] = None) -> str:
 async def webhook(result: int) -> None:
     """Webhook endpoint."""
     logger.info("The webhook was hit, result was: %d", result)
+
+
+@app.post("/test", tags=["test"], operation_id="test")
+async def test(model: MyModel) -> ResModel:
+    """Webhook endpoint."""
+    logger.info("The webhook was hit, result was: %s", model)
+    return ResModel(msg="gucci")
